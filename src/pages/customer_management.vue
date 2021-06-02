8c94f595e647fafbd81c2dcbe66d233b9a851116
newit-hungnb <hungnb@newit.co.jp>
2021-06-02 19:57:23 +0700

    vuejs code

---
+++ src/pages/customer_management.vue
@@ -0,0 +1,170 @@
+<template>
+  <q-page class="q-pa-sm">
+    <q-card>
+      <q-table
+        title="Customer Management"
+        :data="employers"
+        :hide-header="mode === 'grid'"
+        :columns="columns"
+        row-key="name"
+        :grid="mode=='grid'"
+        :filter="filter"
+        :pagination.sync="pagination"
+      >
+        <template v-slot:top-right="props">
+          <q-btn @click="new_employer=true" outline color="primary" label="Add New" class="q-mr-xs"/>
+
+          <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
+            <template v-slot:append>
+              <q-icon name="search"/>
+            </template>
+          </q-input>
+
+          <q-btn
+            flat
+            round
+            dense
+            :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+            @click="props.toggleFullscreen"
+            v-if="mode === 'list'"
+          >
+            <q-tooltip
+              :disable="$q.platform.is.mobile"
+              v-close-popup
+            >{{ props.inFullscreen ? 'Exit Fullscreen' : 'Toggle Fullscreen' }}
+            </q-tooltip>
+          </q-btn>
+
+          <q-btn
+            flat
+            round
+            dense
+            :icon="mode === 'grid' ? 'list' : 'grid_on'"
+            @click="mode = mode === 'grid' ? 'list' : 'grid'; separator = mode === 'grid' ? 'none' : 'horizontal'"
+            v-if="!props.inFullscreen"
+          >
+            <q-tooltip
+              :disable="$q.platform.is.mobile"
+              v-close-popup
+            >{{ mode === 'grid' ? 'List' : 'Grid' }}
+            </q-tooltip>
+          </q-btn>
+        </template>
+        <template v-slot:body-cell-action="props">
+          <q-td :props="props">
+            <div class="q-gutter-sm">
+              <q-btn dense color="primary" icon="edit" @click="update(props.row.name)"/>
+              <q-btn dense color="red" icon="delete"/>
+            </div>
+          </q-td>
+        </template>
+      </q-table>
+    </q-card>
+    <q-dialog v-model="new_employer">
+      <q-card style="width: 600px; max-width: 60vw;">
+        <q-card-section>
+          <div class="text-h6">
+            Add new employer
+            <q-btn round flat dense icon="close" class="float-right" color="grey-8" v-close-popup></q-btn>
+          </div>
+        </q-card-section>
+        <q-separator inset></q-separator>
+        <q-card-section class="q-pt-none">
+          <q-form class="q-gutter-md">
+            <q-list>
+              <q-item>
+                <q-item-section>
+                  <q-item-label class="q-pb-xs">Employer Name</q-item-label>
+                  <q-input dense outlined v-model="employer.name" label="Employer Name"/>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label class="q-pb-xs">Wage</q-item-label>
+                  <q-input dense outlined v-model="employer.wage" label="Wage"/>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-form>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-teal">
+          <q-btn label="Save" type="submit" color="primary" v-close-popup @click="store"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+import axios from 'axios'
+
+function wrapCsvValue(val, formatFn) {
+  let formatted = formatFn !== void 0 ? formatFn(val) : val;
+
+  formatted =
+    formatted === void 0 || formatted === null ? "" : String(formatted);
+
+  formatted = formatted.split('"').join('""');
+
+  return `"${formatted}"`;
+}
+
+export default {
+  data() {
+    return {
+      filter: "",
+      url: "http://localhost:8000/employees",
+      employer: {},
+      new_employer: false,
+      mode: "list",
+      columns: [
+        {
+          name: "id",
+          required: true,
+          label: "Id",
+          align: "left",
+          field: "id",
+          sortable: true
+        },
+        {
+          name: "name",
+          align: "left",
+          label: "Name",
+          field: "name",
+          sortable: true
+        },
+        {
+          name: "wage",
+          align: "left",
+          label: "Wage",
+          field: "wage",
+          sortable: true
+        },
+      ],
+      employers: [],
+      pagination: {
+        rowsPerPage: 10
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    store: function () {
+      axios.post(this.url, {
+        name: this.employer.name,
+        wage: this.employer.wage
+      }).finally(function(){
+        window.location.reload();
+      })
+    },
+    getList: function () {
+      axios.get(this.url).then((response) => {
+        this.employers = response.data
+      });
+    }
+  }
+};
+</script>
